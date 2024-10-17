e23ee6d1440d00ee9c8359d7d2024054035f34fc
Sachin20010517 <sachinayeshmantha@gmail.com>
2024-10-17 06:02:18 +0530

    just updated the karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -9,18 +6,18 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
+            require('karma-coverage'), // use karma-coverage instead of istanbul
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
+        coverageReporter: {
             dir: require('path').join(__dirname, './coverage/atlantis-ng'),
-            reports: ['html', 'lcovonly', 'text-summary'],
+            reporters: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'coverage'], // Add 'coverage' here for the new reporter
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
