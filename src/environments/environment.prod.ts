1af7656c6d0f93d775235266640e3cd2bcf22a32
Sachin20010517 <sachinayeshmantha@gmail.com>
2024-10-17 11:29:30 +0530

    just changed the auth0 configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
   serverUrl: 'https://tender-mgt-tyk.gateway.cgaas.ai',
 
   //AUTH0CLIENTID: 'TDlHgD3IgJSjM9DyKn9gZF1jLCdc0r7A',
-  AUTH0CLIENTID: 'w0h69iNiT9r5E6QSRyuY4UTNIBvFoiyS',
+  AUTH0CLIENTID: 'pSOJ7AxmiOHkqAaROWlHQHMKxkHzmAbe',
   //AUTH0DOMAIN: 'cgaas-generated.eu.auth0.com',
-  AUTH0DOMAIN: 'tender-management.uk.auth0.com',
-  REDIRECTURL: 'https://evolzatendermanagemnt-WS475.demo.cgaas.ai',
+  AUTH0DOMAIN: 'dev-8pa1lpkq8wnkn7ic.us.auth0.com',
+  REDIRECTURL: 'https://tender-mgt-portal.sachinayeshmantha.live',
   CompanyLogo: '../../assets/images/logoTender.png',
   WORKSPACEID: 'WS475',
   paymentUrl: 'http://127.0.0.1:8882',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@ export const environment = {
  
   paymentUrl: 'http://127.0.0.1:8882',
 
-  AUTH0CLIENTID: 'w0h69iNiT9r5E6QSRyuY4UTNIBvFoiyS',
-  AUTH0DOMAIN: 'tender-management.uk.auth0.com',
-  REDIRECTURL: 'https://evolzatendermanagemnt-WS475.demo.cgaas.ai',
+  AUTH0CLIENTID: 'pSOJ7AxmiOHkqAaROWlHQHMKxkHzmAbe',
+  AUTH0DOMAIN: 'dev-8pa1lpkq8wnkn7ic.us.auth0.com',
+  REDIRECTURL: 'https://tender-mgt-portal.sachinayeshmantha.live',
   // CompanyLogo: 'https://evolza-org-bucket.s3.amazonaws.com/Ui-generator/1728070684111419569_NPP_Logo.png.webp',
   CompanyLogo: '../../assets/images/logoTender.png',
 
